4039a33a8c58a6bd5bdc5a40b6e5fad87f9199c3
Jens gerwins <Jens.gerwins@gmail.com>
2023-02-06 18:48:13 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let userName = "Brad";
+userName = "Jenna";
+
+function getUserNameLength() {
+  return  result = name.length;
+}
+
+console.log(getUserNameLength(userName) > 4);
+
+const isString = function (stringz) {
+  return typeof stringz == "string";
+
+}
+console.log(isString("Hello"));
+console.log(isString(3));
+console.log(isString(undefined));
+console.log(isString(""));
+console.log(isString("john" + "Doe"));
